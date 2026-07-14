c93ac7d67a564d2c415a95644d985e511b43f758
Ww <wwwwesleysantdasilva@gmail.com>
2026-07-14 08:15:28 -0300

    Delete events/clientready.js

--- events/clientready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'clientReady',
-    once: true,
-    execute(client) {
-        console.log(`✅ Bot de limpeza ativo como ${client.user.tag} (Estrutura Modular + Container V2)`);
-    },
-};
